60b1a9b10b8de6361564b3adea806875b2b42de1
406892700 <406892700@qq.com>
2015-01-29 18:01:26 +0800

    今天就加了一些辅助性的功能

--- lierature/routes/index.js
+++ lierature/routes/index.js
@@ -10,18 +10,69 @@
 var userDao = require('../dao/userDao');
 
 module.exports = function(app){
+    //首页
     app.get("/",function(req,res){
         console.log(req.session.user.username);
         res.render("index/index",{'title':'1234s','user':req.session.user});
     });
-
+    //注册页
     app.get("/reg",function(req,res){
         res.render('register/reg',{'name':'欢迎注册本站'});
     });
 
-
+    //登陆页
     app.get("/login",function(req,res){
         res.render('login/login',{'title':"登录"});
+    });
+
+    //后台错误 500
+    app.get("/error_500",function(req,res){
+        res.render('error/500',{});
+    });
+
+    //路径错误 404
+
+    app.get('/error_404',function(req,res){
+        res.render('error/404',{});
+    });
+
+
+
+    //test
+
+    app.get('/sendMail',function(req,res){
+        res.render('sendMail',{});
+    });
+
+    app.get('/send_Mail',function(req,res){
+        var nodemailer = require('nodemailer');
+
+        //创建一个可以重用的SMTP邮件发送对象
+        var transporter = nodemailer.createTransport({
+            service:'qq',//smtp服务商
+            auth: {
+                user: '406892700',//用户名
+                pass: 'xhy15057142408'//密码
+            }
+        });
+
+        //配置一个发送
+        var mailOptions = {
+            from: 'admin<406892700@qq.com>', //发送者邮箱
+            to: '2945371399@qq.com', // 接收者邮箱
+            subject: '文学网站用户注册邮箱验证', //
+            text: '请点击下面的链接已完成注册', // 邮件的文字内容
+            html: "<a>"+ddd+"</a>" // 邮件的html部分内容
+        };
+
+        //用已经定义的邮件传送对象来发送邮件
+        transporter.sendMail(mailOptions, function(error, info){
+            if(error){
+                console.log(error);
+            }else{
+                console.log('Message sent: ' + info.response);
+            }
+        });
     });
 
 }
--- lierature/routes/log_reg.js
+++ lierature/routes/log_reg.js
@@ -8,28 +8,25 @@ module.exports = function(app){
     //登录
     app.post("/loginNow",function(req,res){
         var user = req.body;
-//        userDao.ifExist(user,function(err,flag){
-//            //console.log(!!(err && flag.length == 0));
-//            if(!!(!err && flag.length == 0)){
-//                res.json({'info':'login failed!'});
-//            }else{
-//                res.json({'info':'login success!'});
-//                req.session.user = user;
-//                res.redirect("/");
-//            }
-//        });
         var md5 = crypto.createHash('md5');
         var password = md5.update(user.password).digest('hex');
         userDao.login(user,function(err,docs){
-            console.log(docs);
-
-
+            console.log(docs.length);
+            if(err)
+                res.redirect('/error_500');
+            else if(docs.length == 0){
+                //console.log('账号或密码错误！');
+                res.redirect('/login')
+            }else{
+                //console.log('登录成功!');
+                req.session.user = user;//写入session
+                res.redirect('/');
+            }
         });
     });
 
     //用户名的唯一性验证
     app.get("/check_exist",function(req,res){
-        //console.log("---------------"+req.query.username);
         userDao.ifExist({'username':req.query.username},function(err,flag){
             !!(!err && flag.length == 0)?res.json({'code':'0'}):res.json({'code':'1'});
         });
@@ -46,4 +43,6 @@ module.exports = function(app){
             err?res.json('info','register faild!'):res.json({'info':'register success!'});
         });
     });
+
+
 }
